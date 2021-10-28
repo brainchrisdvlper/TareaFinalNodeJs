b6369974b421c31acce41041e66bcc818800acfc
= <brainchrisdvlper@gmail.com>
2021-10-27 23:53:08 -0300

    primer commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const compression = require("compression");
 const path = require("path");
 const app = express();
-app.use(express.static("public"));
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, "build")));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
