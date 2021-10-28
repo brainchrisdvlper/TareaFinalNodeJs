043882cb466516cadc03ba8695a4d495f065807b
= <brainchrisdvlper@gmail.com>
2021-10-27 23:59:01 -0300

    primer commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 //app.get("*", function (req, res) {
 //  res.sendFile(path.join(__dirname, "public", "index.html"));
